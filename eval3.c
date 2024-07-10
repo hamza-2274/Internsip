b4539b602ede77cf08f50d5659b0bbd051e6f320
hamza-2274 <145056354+hamza-2274@users.noreply.github.com>
2024-07-10 14:49:02 +0500

    Update eval3.c

--- eval3.c
+++ eval3.c
@@ -1,16 +1,1 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stdint.h>
-
-
-void divide(uint32_t dividend, uint32_t divisor, uint32_t *quotient, uint32_t *remainder) {
-    if (divisor == 0) {
-        printf("error  by zero\n");
-        *quotient = 0;
-        *remainder = 0;
-        return;
-    }
-    *quotient = dividend / divisor;
-    *remainder = dividend % divisor;}
