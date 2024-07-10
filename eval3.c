bc575efb176f00491fddcd075977a5c25c8ae49f
hamza-2274 <145056354+hamza-2274@users.noreply.github.com>
2024-07-10 14:52:02 +0500

    Update eval3.c

--- eval3.c
+++ eval3.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void divide(int dividend, int divisor, int *quotient, int *remainder) {
+    int a = 0;
+    int q = dividend;
+    int m = divisor;
+    int n = sizeof(int) * 8;
+
+    for (int i = 0; i < n; i++) {
+        a = (a << 1) | ((q & (1 << (n - 1))) >> (n - 1));
+        q = q << 1;
+        a = a - m;
+
+        if (a < 0) {
+            q = q & ~(1);
+            a = a + m;
+        } else {
+            q = q | 1;
+        }
+    }
+
+    *quotient = q;
+    *remainder = a;
+}
+
+int main() {
+    int dividend, divisor;
+    printf("rnter dividend: ");
+    scanf("%d", &dividend);
+    printf("enter divisor: ");
+    scanf("%d", &divisor);
+
+    int quotient, remainder;
+    divide(dividend, divisor, &quotient, &remainder);
+
+    printf("Quotient: %d\n", quotient);
+    printf("Remainder: %d\n", remainder);
+
+    return 0;
+}
 
