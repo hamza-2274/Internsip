9243bc39892c66a97ea4d273e78faeee805929c5
hamza-2274 <145056354+hamza-2274@users.noreply.github.com>
2024-07-10 14:59:40 +0500

    Update eval3.c

--- eval3.c
+++ eval3.c
@@ -24,19 +24,4 @@ void divide(int dividend, int divisor, int *quotient, int *remainder) {
     *remainder = a;
 }
 
-int main() {
-    int dividend, divisor;
-    printf("rnter dividend: ");
-    scanf("%d", &dividend);
-    printf("enter divisor: ");
-    scanf("%d", &divisor);
-
-    int quotient, remainder;
-    divide(dividend, divisor, &quotient, &remainder);
-
-    printf("Quotient: %d\n", quotient);
-    printf("Remainder: %d\n", remainder);
-
-    return 0;
-}
 
